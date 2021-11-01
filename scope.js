f69db2102edb7935999653c0dc678c16101c8661
Amit Sharma <85213380+AmitStark@users.noreply.github.com>
2021-11-01 19:15:58 +0530

    Update scope.js

--- scope.js
+++ scope.js
@@ -1,12 +1,9 @@
-var message = "in global";
-console.log("global: message =" + message);
-var a = function ()  {
-var message = "inside a";
-console.log("a: message =" + message);
-b(); }
+var message = 'Hi';
 
-function b()  {
-console.log("b: message =" + message);
+function say() {
+    var message = 'Hello';
+    console.log('message');
 }
-a();
- 
+
+say();
+console.log(message);
