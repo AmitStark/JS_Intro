0167b742cdc752433fae46760cce1a83b5b8b5b4
Amit Sharma <85213380+AmitStark@users.noreply.github.com>
2021-11-01 19:27:11 +0530

    Create lscope.js

---
+++ lscope.js
@@ -0,0 +1,14 @@
+<script>
+    
+    function createUserName() {
+        var userName = "Rainav";
+    }
+
+    function showUserName() {
+        alert(userName);
+    }
+
+    createUserName();
+    showUserName(); // throws error: userName is not defined
+
+</script>
